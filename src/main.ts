b965a45aab8bb89ee5faeeb6dd4d65325b66d297
Florian Kelnerowski <fkelnero@bouyguestelecom.fr>
2022-05-18 11:56:13 +0200

    console log first test

--- src/main.ts
+++ src/main.ts
@@ -3,57 +3,38 @@ import {AppointmentModel} from './models/appointment.model';
 import {DayAvailabilitiesModel, SlotAvailabilities} from './models/dayAvailabilities.model';
 
 const companyAvailabilitiesList: CompanyAvailabilitiesModel[] = [];
-const dayAvailabilitiesList: DayAvailabilitiesModel[] = [];
-let availability: CompanyAvailabilitiesModel;
-
-// recurring schedule of a company
-let startDate = new Date(2016, 6, 1, 10, 30); // July 1st, 10:30
-let endDate = new Date(2016, 6, 1, 14, 0o0); // July 1st, 14:00
-// eslint-disable-next-line max-len
-availability = new CompanyAvailabilitiesModel(true, true, startDate, endDate); // weekly recurring opening in calendar
-companyAvailabilitiesList.push(availability);
-
-// Single appoinment taken of company
-startDate = new Date(2016, 6, 8, 11, 30); // July 8th 11:30
-endDate = new Date(2016, 6, 8, 12, 30); // July 8th 12:30
-// eslint-disable-next-line max-len
-availability = new CompanyAvailabilitiesModel(false, false, startDate, endDate);
-companyAvailabilitiesList.push(availability);
-
-// Appointment from a customer
-const fromDate = new Date(2016, 6, 4, 10, 0o0); // July 4th 10:00
-const toDate = new Date(2016, 6, 10, 10, 0o0); // July 10th 10:00
-const appointment = new AppointmentModel(fromDate, toDate);
-
-calculateAvailabilities(companyAvailabilitiesList, appointment);
 
 // eslint-disable-next-line require-jsdoc
-function calculateAvailabilities(
-    companyAvailabilitiesList: CompanyAvailabilitiesModel[],
-    appointment: AppointmentModel) : void {
+export function calculateAvailabilities(appointment: AppointmentModel) : void {
+  let dayAvailabilitiesList: DayAvailabilitiesModel[] = [];
   for (const availability of companyAvailabilitiesList) {
     // eslint-disable-next-line max-len
     // 1) Add all available company appointments to an array (recurring & ordering == true)
     if (availability.opening === true && availability.recurring === true) {
-      addCompanyRecurrentAvailibilities(appointment, availability);
+      dayAvailabilitiesList = addCompanyRecurrentAvailibilities(appointment, availability);
     } else if (availability.opening === false && availability.recurring === false) {
       // 2) Change the appointments from dayAvailabilitiesList already taken using availability (recurring & ordering == false)
-      updateDayAvailabilitiesList(availability);
+      dayAvailabilitiesList = updateDayAvailabilitiesList(availability, dayAvailabilitiesList);
     }
   }
   // 3) Return the appointments available from the company
-  displayAvailabilityList(appointment);
+  displayAvailabilityList(appointment, dayAvailabilitiesList);
+}
+
+export function addToCompanyAvailabilitiesList(opening: boolean, recurring: boolean, startDate: Date, endDate: Date) : void {
+  const availability: CompanyAvailabilitiesModel= new CompanyAvailabilitiesModel(opening, recurring, startDate, endDate);
+  companyAvailabilitiesList.push(availability);
 }
 
 // Generate output text to display
-function displayAvailabilityList(appointment: AppointmentModel) : void {
+function displayAvailabilityList(appointment: AppointmentModel, dayAvailabilitiesList: DayAvailabilitiesModel[]) : void {
   let outPutText: string;
   let outPutSlots: SlotAvailabilities[];
   const appointmentCopy: AppointmentModel = appointment;
   while (appointmentCopy.fromDate.getDay() !== appointmentCopy.toDate.getDay()) {
     for (const day of dayAvailabilitiesList) {
       if (day.dayNumber === appointmentCopy.fromDate.getDay()) {
-        outPutText = `* I\'m available from ${appointmentCopy.fromDate.toLocaleString('en-US', {month: 'long'})}`;
+        outPutText = `I'm available from ${appointmentCopy.fromDate.toLocaleString('en-US', {month: 'long'})}`;
         outPutText+= ` ${appointmentCopy.fromDate.getDate()}, at `;
         outPutSlots = day.slot.filter((slot) => slot.status == true);
         for (const [key, slot] of Object.entries(outPutSlots)) {
@@ -70,7 +51,7 @@ function displayAvailabilityList(appointment: AppointmentModel) : void {
   }
 }
 
-function updateDayAvailabilitiesList(availability : CompanyAvailabilitiesModel) : void {
+function updateDayAvailabilitiesList(availability : CompanyAvailabilitiesModel, dayAvailabilitiesList: DayAvailabilitiesModel[]) : DayAvailabilitiesModel[] {
   const appointmentDayNumber: number = availability.startDate.getDay();
   const startHour: string = formatStartHour(availability);
   const originalDiffTime: number = Math.abs(availability.startDate.getTime() - availability.endDate.getTime()) / 36e5;
@@ -90,6 +71,7 @@ function updateDayAvailabilitiesList(availability : CompanyAvailabilitiesModel) 
       }
     }
   }
+  return dayAvailabilitiesList;
 }
 
 function addZeroAfter(n: number) : string {
@@ -107,8 +89,9 @@ function formatEndHour(availability: CompanyAvailabilitiesModel) : string {
 // Add to dayAvailabilitiesList the number of day concerned and a list of appointments with a status equal true
 function addCompanyRecurrentAvailibilities(
     appointment : AppointmentModel,
-    availability : CompanyAvailabilitiesModel) : void {
+    availability : CompanyAvailabilitiesModel) : DayAvailabilitiesModel[] {
   const dayAvailabilities: DayAvailabilitiesModel = new DayAvailabilitiesModel();
+  const dayAvailabilitiesList: DayAvailabilitiesModel[] = [];
   dayAvailabilities.dayNumber = availability.startDate.getDay();
   let startHour: string = formatStartHour(availability);
   const endHour: string = formatEndHour(availability);
@@ -129,4 +112,5 @@ function addCompanyRecurrentAvailibilities(
   }
   dayAvailabilities.slot = slotsList;
   dayAvailabilitiesList.push(dayAvailabilities);
+  return dayAvailabilitiesList;
 }
--- src/test/main.test.js
+++ src/test/main.test.js
@@ -1,10 +1,27 @@
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
+import {CompanyAvailabilitiesModel} from '../models/companyAvailabilities.model';
+import {addToCompanyAvailabilitiesList, calculateAvailabilities} from '../main';
+const {AppointmentModel} = require('../models/appointment.model');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('console.log * I\'m available from July 8, at 10:30, 11:00, 12:30, 13:00, and 13:30\n', () => {
+  // recurring schedule of a company
+  let startDate = new Date(2016, 6, 1, 10, 30); // July 1st, 10:30
+  let endDate = new Date(2016, 6, 1, 14, 0o0); // July 1st, 14:00
+  addToCompanyAvailabilitiesList(true, true, startDate, endDate);
+
+  // Single appoinment taken of company
+  startDate = new Date(2016, 6, 8, 11, 30); // July 8th 11:30
+  endDate = new Date(2016, 6, 8, 12, 30); // July 8th 12:30
+  addToCompanyAvailabilitiesList(false, false, startDate, endDate);
+
+  // Appointment from a customer
+  const fromDate = new Date(2016, 6, 4, 10, 0o0); // July 4th 10:00
+  const toDate = new Date(2016, 6, 10, 10, 0o0); // July 10th 10:00
+  const appointment = new AppointmentModel(fromDate, toDate);
+
+  const consoleSpy = jest.spyOn(console, 'log');
+
+  calculateAvailabilities(appointment);
+
+  expect(consoleSpy).toHaveBeenCalledWith(`I'm available from July 8, at 10:30, 11:00, 12:30, 13:00, and 13:30`);
 });
-
 
